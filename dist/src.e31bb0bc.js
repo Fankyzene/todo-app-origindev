7669294ae6484f5ec6edf3e6778233313d84c451
Fankyzene <marjai.janos@gmail.com>
2022-11-10 11:33:37 +0100

    added buttons

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -28911,17 +28911,39 @@ function TodoForm(props) {
     ref: inputRef
   }), /*#__PURE__*/_react.default.createElement("button", {
     className: "todo-button edit"
-  }, "Update")) : /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("input", {
+  }, /*#__PURE__*/_react.default.createElement("svg", {
+    class: "w-6 h-6",
+    fill: "none",
+    stroke: "currentColor",
+    viewBox: "0 0 24 24",
+    xmlns: "http://www.w3.org/2000/svg"
+  }, /*#__PURE__*/_react.default.createElement("path", {
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
+    "stroke-width": "2",
+    d: "M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+  })))) : /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("input", {
     type: "text",
     placeholder: "Add todo",
     value: input,
     name: "text",
-    class: "max-w-md",
+    class: "max-w-md mb-2",
     onChange: handleChange,
     ref: inputRef
   }), /*#__PURE__*/_react.default.createElement("button", {
-    class: "max-w-md"
-  }, "Add todo")));
+    class: "max-w-md text-2xl pl-2.5 "
+  }, /*#__PURE__*/_react.default.createElement("svg", {
+    class: "w-6 h-6",
+    fill: "none",
+    stroke: "currentColor",
+    viewBox: "0 0 24 24",
+    xmlns: "http://www.w3.org/2000/svg"
+  }, /*#__PURE__*/_react.default.createElement("path", {
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
+    "stroke-width": "2",
+    d: "M12 6v6m0 0v6m0-6h6m-6 0H6"
+  })))));
 }
 var _default = TodoForm;
 exports.default = _default;
@@ -28980,13 +29002,24 @@ function Todo(_ref) {
         return completeTodo(todo.id);
       }
     }, todo.text), /*#__PURE__*/_react.default.createElement("div", {
-      class: "flex flex-row "
+      class: "flex flex-row gap-1"
     }, /*#__PURE__*/_react.default.createElement("div", {
       className: "delete-icon",
       onClick: function onClick() {
         return removeTodo(todo.id);
       }
-    }, "remove"), /*#__PURE__*/_react.default.createElement("div", {
+    }, /*#__PURE__*/_react.default.createElement("svg", {
+      class: "w-6 h-6",
+      fill: "none",
+      stroke: "currentColor",
+      viewBox: "0 0 24 24",
+      xmlns: "http://www.w3.org/2000/svg"
+    }, /*#__PURE__*/_react.default.createElement("path", {
+      "stroke-linecap": "round",
+      "stroke-linejoin": "round",
+      "stroke-width": "2",
+      d: "M6 18L18 6M6 6l12 12"
+    }))), /*#__PURE__*/_react.default.createElement("div", {
       className: "edit-icon",
       onClick: function onClick() {
         return setEdit({
@@ -28994,7 +29027,18 @@ function Todo(_ref) {
           value: todo.text
         });
       }
-    }, "edit"))));
+    }, /*#__PURE__*/_react.default.createElement("svg", {
+      class: "w-6 h-6",
+      fill: "none",
+      stroke: "currentColor",
+      viewBox: "0 0 24 24",
+      xmlns: "http://www.w3.org/2000/svg"
+    }, /*#__PURE__*/_react.default.createElement("path", {
+      "stroke-linecap": "round",
+      "stroke-linejoin": "round",
+      "stroke-width": "2",
+      d: "M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+    }))))));
   });
 }
 var _default = Todo;
--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -36,7 +36,22 @@ function TodoForm(props, ...todos) {
             onChange={handleChange}
             ref={inputRef}
           />
-          <button className="todo-button edit">Update</button>
+          <button className="todo-button edit">
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+              ></path>
+            </svg>
+          </button>
         </>
       ) : (
         <>
@@ -45,12 +60,27 @@ function TodoForm(props, ...todos) {
             placeholder="Add todo"
             value={input}
             name="text"
-            class="max-w-md"
+            class="max-w-md mb-2"
             onChange={handleChange}
             ref={inputRef}
           />
 
-          <button class="max-w-md">Add todo</button>
+          <button class="max-w-md text-2xl pl-2.5 ">
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              ></path>
+            </svg>
+          </button>
         </>
       )}
     </form>
